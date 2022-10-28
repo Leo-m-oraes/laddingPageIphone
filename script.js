b10a771412e11afd4960aacd7329885298d45a96
Leonardo Moraes <leonardomoraes1504@outlook.com>
2022-10-28 15:20:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(e){
+    document.querySelector('#phone').src = e;
+}
+function circleChange(color){
+   const circle = document.querySelector('.circle');
+   circle.style.background = color
+}
